9d7d8542791519be2778aa93f9d2c5879dd6afcd
Martin <x00135214@gmail.com>
2021-05-16 16:28:29 +0100

    6 values validation added

--- run.py
+++ run.py
@@ -29,7 +29,26 @@ def get_sales_data():
     print("Data should be six numbers, separated by commas.")
     print("Example: 10, 20, 30, 40, 50, 60\n")
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str} ")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+    #print(sales_data)
+    #print(f"The data provided is {data_str} ")
+
+def validate_data(values):
+    """
+    Validate sales data entered
+    Inside the try, converts all string values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+            f'Exactly six values required, you provided {len(values)}'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again\n')
+
 
 get_sales_data()
 # run in command line - python3 run.py
+
+ 
